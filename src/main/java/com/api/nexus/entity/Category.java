8bf579beed7eeb4adc16205539dc5e2368bb3a26
DaikoDev <cdanieleon28@gmail.com>
2024-09-04 18:16:56 -0500

    Codificar entity de category

--- src/main/java/com/api/nexus/entity/Category.java
+++ src/main/java/com/api/nexus/entity/Category.java
@@ -1,4 +1,20 @@
 package com.api.nexus.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
 }
