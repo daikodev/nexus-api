6d3bd08c032767b1185d7d5e555278a067c92a93
DaikoDev <cdanieleon28@gmail.com>
2024-09-04 18:18:36 -0500

    Codificar controller de product

--- src/main/java/com/api/nexus/controller/ProductController.java
+++ src/main/java/com/api/nexus/controller/ProductController.java
@@ -1,4 +1,22 @@
 package com.api.nexus.controller;
 
+import com.api.nexus.entity.Product;
+import com.api.nexus.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("nexus/products")
 public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
 }
