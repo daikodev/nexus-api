75bf0c8673427af52e3ff1a0de8fb77820afc6de
DaikoDev <cdanieleon28@gmail.com>
2024-09-04 18:18:04 -0500

    Codificar service de product

--- src/main/java/com/api/nexus/service/ProductService.java
+++ src/main/java/com/api/nexus/service/ProductService.java
@@ -1,4 +1,19 @@
 package com.api.nexus.service;
 
+import com.api.nexus.entity.Product;
+import com.api.nexus.repository.ProductRepository;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
 }
