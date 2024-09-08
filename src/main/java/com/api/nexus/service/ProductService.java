a42229280788ec8a1be5858d128b7b6cb06f0b6c
DaikoDev <cdanieleon28@gmail.com>
2024-09-08 00:57:46 -0500

    Crear servicio para obtener paginacion de productos

--- src/main/java/com/api/nexus/service/ProductService.java
+++ src/main/java/com/api/nexus/service/ProductService.java
@@ -1,21 +1,28 @@
 package com.api.nexus.service;
 
 import com.api.nexus.entity.Product;
+import com.api.nexus.pagination.PageableQuery;
 import com.api.nexus.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    public List<Product> getAllProducts() {
-        return productRepository.findAll();
+    public Page<Product> getProductsByPage(PageableQuery pageableQuery) {
+        Sort sort = Sort.by(Sort.Direction.fromString(pageableQuery.getInOrder()),
+                pageableQuery.getOrderBy());
+
+        Pageable pageable = PageRequest.of(pageableQuery.getPage(),
+                pageableQuery.getItemsByPage(), sort);
+
+        return productRepository.findAll(pageable);
     }
 
     public Product saveProduct(Product product) {
